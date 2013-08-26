paulirish/devtools-frontend-2013-2015

commit d40d0fbddc9054c02097355b01dab16f9f5d4df1

DevTools: Front-end for platform font inspection
This patch adds UI for the 'platform font inspection' feature.

BUG=135489

Review URL: https://chromiumcodereview.appspot.com/22875023

git-svn-id: svn://svn.chromium.org/blink/trunk@156700 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/scripts/compile_frontend.py
@@ -208,6 +208,7 @@ modules = [
             "ElementsPanelDescriptor.js",
             "EventListenersSidebarPane.js",
             "MetricsSidebarPane.js",
+            "PlatformFontsSidebarPane.js"
             "PropertiesSidebarPane.js",
             "StylesSidebarPane.js",
         ]

Source/devtools/devtools.gyp
@@ -243,6 +243,7 @@
             'front_end/ElementsPanel.js',
             'front_end/EventListenersSidebarPane.js',
             'front_end/MetricsSidebarPane.js',
+            'front_end/PlatformFontsSidebarPane.js',
             'front_end/PropertiesSidebarPane.js',
             'front_end/StylesSidebarPane.js',
         ],

Source/devtools/front_end/CSSStyleModel.js
@@ -159,6 +159,22 @@ WebInspector.CSSStyleModel.prototype = {
     },
 
     /**
+     * @param {number} nodeId
+     * @param {function(?String, ?Array.<CSSAgent.PlatformFontUsage>)} callback
+     */
+    getPlatformFontsForNode: function(nodeId, callback)
+    {
+        function platformFontsCallback(error, cssFamilyName, fonts)
+        {
+            if (error)
+                callback(null, null);
+            else
+                callback(cssFamilyName, fonts);
+        }
+        CSSAgent.getPlatformFontsForNode(nodeId, platformFontsCallback);
+    },
+
+    /**
      * @param {DOMAgent.NodeId} nodeId
      * @param {function(?WebInspector.CSSStyleDeclaration, ?WebInspector.CSSStyleDeclaration)} userCallback
      */

Source/devtools/front_end/ElementsPanel.js
@@ -32,6 +32,7 @@ importScript("CSSNamedFlowCollectionsView.js");
 importScript("CSSNamedFlowView.js");
 importScript("EventListenersSidebarPane.js");
 importScript("MetricsSidebarPane.js");
+importScript("PlatformFontsSidebarPane.js");
 importScript("PropertiesSidebarPane.js");
 importScript("StylesSidebarPane.js");
 
@@ -79,6 +80,7 @@ WebInspector.ElementsPanel = function()
     this.crumbsElement.addEventListener("mouseout", this._mouseMovedOutOfCrumbs.bind(this), false);
 
     this.sidebarPanes = {};
+    this.sidebarPanes.platformFonts = new WebInspector.PlatformFontsSidebarPane();
     this.sidebarPanes.computedStyle = new WebInspector.ComputedStyleSidebarPane();
     this.sidebarPanes.styles = new WebInspector.StylesSidebarPane(this.sidebarPanes.computedStyle, this._setPseudoClassForNodeId.bind(this));
     this.sidebarPanes.metrics = new WebInspector.MetricsSidebarPane();
@@ -88,6 +90,7 @@ WebInspector.ElementsPanel = function()
 
     this.sidebarPanes.styles.addEventListener(WebInspector.SidebarPane.EventTypes.wasShown, this.updateStyles.bind(this, false));
     this.sidebarPanes.metrics.addEventListener(WebInspector.SidebarPane.EventTypes.wasShown, this.updateMetrics.bind(this));
+    this.sidebarPanes.platformFonts.addEventListener(WebInspector.SidebarPane.EventTypes.wasShown, this.updatePlatformFonts.bind(this));
     this.sidebarPanes.properties.addEventListener(WebInspector.SidebarPane.EventTypes.wasShown, this.updateProperties.bind(this));
     this.sidebarPanes.eventListeners.addEventListener(WebInspector.SidebarPane.EventTypes.wasShown, this.updateEventListeners.bind(this));
 
@@ -233,6 +236,7 @@ WebInspector.ElementsPanel.prototype = {
 
         this.updateStyles(true);
         this.updateMetrics();
+        this.updatePlatformFonts();
         this.updateProperties();
         this.updateEventListeners();
     },
@@ -586,6 +590,8 @@ WebInspector.ElementsPanel.prototype = {
         // Once styles are edited, the Metrics pane should be updated.
         this.sidebarPanes.metrics.needsUpdate = true;
         this.updateMetrics();
+        this.sidebarPanes.platformFonts.needsUpdate = true;
+        this.updatePlatformFonts();
     },
 
     _metricsPaneEdited: function()
@@ -997,6 +1003,16 @@ WebInspector.ElementsPanel.prototype = {
         metricsSidebarPane.needsUpdate = false;
     },
 
+    updatePlatformFonts: function()
+    {
+        var platformFontsSidebar = this.sidebarPanes.platformFonts;
+        if (!platformFontsSidebar.isShowing() || !platformFontsSidebar.needsUpdate)
+            return;
+
+        platformFontsSidebar.update(this.selectedDOMNode());
+        platformFontsSidebar.needsUpdate = false;
+    },
+
     updateProperties: function()
     {
         var propertiesSidebarPane = this.sidebarPanes.properties;
@@ -1147,6 +1163,8 @@ WebInspector.ElementsPanel.prototype = {
         computedPane.bodyElement.addStyleClass("metrics-and-computed");
         this.sidebarPanes.computedStyle.show(computedPane.bodyElement);
         this.sidebarPanes.computedStyle.setExpandCallback(expandComputed);
+
+        this.sidebarPanes.platformFonts.show(computedPane.bodyElement);
 
         if (vertically) {
             this.sidebarPaneView = new WebInspector.SidebarTabbedPane();

Source/devtools/front_end/PlatformFontsSidebarPane.js
@@ -1,0 +1,116 @@
+/*
+ * Copyright (C) 2013 Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @constructor
+ * @extends {WebInspector.SidebarPane}
+ */
+WebInspector.PlatformFontsSidebarPane = function()
+{
+    WebInspector.SidebarPane.call(this, WebInspector.UIString("Fonts"));
+    this.element.addStyleClass("platform-fonts");
+    WebInspector.domAgent.addEventListener(WebInspector.DOMAgent.Events.AttrModified, this._onNodeChange.bind(this));
+    WebInspector.domAgent.addEventListener(WebInspector.DOMAgent.Events.AttrRemoved, this._onNodeChange.bind(this));
+    WebInspector.domAgent.addEventListener(WebInspector.DOMAgent.Events.CharacterDataModified, this._onNodeChange.bind(this));
+
+    var cssFontSection = this.element.createChild("div", "stats-section monospace");
+    var cssFontPrefix = cssFontSection.createChild("span", "webkit-css-property");
+    cssFontPrefix.textContent = "font-family";
+    cssFontSection.createTextChild(":");
+    this._cssFontValue = cssFontSection.createChild("span", "css-font-value");
+
+    this._fontStatsSection = this.element.createChild("div", "stats-section");
+}
+
+WebInspector.PlatformFontsSidebarPane.prototype = {
+    _onNodeChange: function()
+    {
+        if (this._innerUpdateTimeout)
+            return;
+        this._innerUpdateTimeout = setTimeout(this._innerUpdate.bind(this), 100);
+    },
+
+    /**
+     * @param {WebInspector.DOMNode=} node
+     */
+    update: function(node)
+    {
+        if (!node) {
+            delete this._node;
+            return;
+        }
+        this._node = node;
+        this._innerUpdate();
+    },
+
+    _innerUpdate: function()
+    {
+        if (this._innerUpdateTimeout) {
+            clearTimeout(this._innerUpdateTimeout);
+            delete this._innerUpdateTimeout;
+        }
+        if (!this._node)
+            return;
+        WebInspector.cssModel.getPlatformFontsForNode(this._node.id, this._refreshUI.bind(this, this._node));
+    },
+
+    /**
+     * @param {String} cssFamilyName
+     * @param {WebInspector.DOMNode} node
+     */
+    _refreshUI: function(node, cssFamilyName, platformFonts)
+    {
+        if (this._node !== node)
+            return;
+        this._cssFontValue.textContent = cssFamilyName + ";";
+        this._fontStatsSection.removeChildren();
+
+        if (!platformFonts || !platformFonts.length)
+            return;
+        platformFonts.sort(function (a, b) {
+            return b.glyphCount - a.glyphCount;
+        });
+        for (var i = 0; i < platformFonts.length; ++i) {
+            var fontStatElement = this._fontStatsSection.createChild("div", "font-stats-item");
+
+            var fontNameElement = fontStatElement.createChild("span", "font-name");
+            fontNameElement.textContent = platformFonts[i].familyName;
+
+            var fontDelimeterElement = fontStatElement.createChild("span", "delimeter");
+            fontDelimeterElement.textContent = "\u2014";
+
+            var fontUsageElement = fontStatElement.createChild("span", "font-usage");
+            var usage = platformFonts[i].glyphCount;
+            fontUsageElement.textContent = usage === 1 ? WebInspector.UIString("%d glyph", usage) : WebInspector.UIString("%d glyphs", usage);
+        }
+    },
+
+    __proto__: WebInspector.SidebarPane.prototype
+}

Source/devtools/front_end/elementsPanel.css
@@ -541,7 +541,6 @@
     -webkit-user-drag: auto;
 }
 
-
 .sidebar-pane.composite {
     overflow: hidden;
     position: absolute;
@@ -560,10 +559,35 @@
     -webkit-justify-content: center;
 }
 
+.sidebar-pane.composite .computed-style {
+    border-bottom: 1px solid rgb(64%, 64%, 64%);
+}
+
 .sidebar-pane.composite .metrics-and-computed .sidebar-pane-toolbar {
     margin-top: 4px;
     margin-bottom: -4px;
     position: relative;
+}
+
+.sidebar-pane.composite .platform-fonts {
+    padding: 1ex;
+    -webkit-user-select: text;
+}
+
+.sidebar-pane.composite .platform-fonts .stats-section {
+    margin-bottom: 5px;
+}
+
+.sidebar-pane.composite .platform-fonts .css-font-value {
+    margin-left: 1ex;
+}
+
+.sidebar-pane.composite .platform-fonts .font-stats-item {
+    padding-left: 1em;
+}
+
+.sidebar-pane.composite .platform-fonts .font-stats-item .delimeter {
+    margin: 0 1ex 0 1ex;
 }
 
 .sidebar-pane.composite .sidebar-pane-toolbar > .sidebar-pane-subtitle {

commit 024fed3247afe5365ab08fb9be785a9beae762c9

DevTools: fixed compile_frontend.py broken by r156700
TBR=lushnikov

Review URL: https://codereview.chromium.org/23190049

git-svn-id: svn://svn.chromium.org/blink/trunk@156710 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/scripts/compile_frontend.py
@@ -208,7 +208,7 @@ modules = [
             "ElementsPanelDescriptor.js",
             "EventListenersSidebarPane.js",
             "MetricsSidebarPane.js",
-            "PlatformFontsSidebarPane.js"
+            "PlatformFontsSidebarPane.js",
             "PropertiesSidebarPane.js",
             "StylesSidebarPane.js",
         ]