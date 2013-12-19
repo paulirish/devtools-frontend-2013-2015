paulirish/devtools-frontend-2013-2015

commit 01cff1a41e66cae650bdaf9446ee35c52568af7f

Timeline: move TimelineWindowFilter to TimelinePanel.js and remove its dependency from TimelineOverviewPane.
DriveBy: move creation of presentation filter into _createFilters method

BUG=
R=caseq@chromium.org

Review URL: https://codereview.chromium.org/115103003

git-svn-id: svn://svn.chromium.org/blink/trunk@163960 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/TimelinePanel.js
@@ -75,8 +75,8 @@ WebInspector.TimelinePanel = function()
 
     // Create presentation model.
     this._presentationModel = new WebInspector.TimelinePresentationModel();
-    this._presentationModel.addFilter(new WebInspector.TimelineWindowFilter(this._overviewPane));
-    this._presentationModel.addFilter(new WebInspector.TimelineCategoryFilter());
+    this._presentationModel.addFilter(this._windowFilter);
+    this._presentationModel.addFilter(this._categoryFilter);
     this._presentationModel.addFilter(this._durationFilter);
     this._presentationModel.setGlueRecords(this._glueParentButton.toggled);
 
@@ -557,9 +557,9 @@ WebInspector.TimelinePanel.prototype = {
      */
     _createFilters: function(filterBar)
     {
-        this._textFilter = new WebInspector.TextFilterUI();
-        this._textFilter.addEventListener(WebInspector.FilterUI.Events.FilterChanged, this._textFilterChanged, this);
-        filterBar.addFilter(this._textFilter);
+        this._textFilterUI = new WebInspector.TextFilterUI();
+        this._textFilterUI.addEventListener(WebInspector.FilterUI.Events.FilterChanged, this._textFilterChanged, this);
+        filterBar.addFilter(this._textFilterUI);
 
         var durationOptions = [];
         for (var presetIndex = 0; presetIndex < WebInspector.TimelinePanel.durationFilterPresetsMs.length; ++presetIndex) {
@@ -575,12 +575,12 @@ WebInspector.TimelinePanel.prototype = {
             durationOption.value = durationMs;
             durationOptions.push(durationOption);
         }
+        this._durationFilterUI = new WebInspector.ComboBoxFilterUI(durationOptions);
+        this._durationFilterUI.addEventListener(WebInspector.FilterUI.Events.FilterChanged, this._durationFilterChanged, this);
+        filterBar.addFilter(this._durationFilterUI);
         this._durationFilter = new WebInspector.TimelineIsLongFilter();
-        this._durationComboBoxFilter = new WebInspector.ComboBoxFilterUI(durationOptions);
-        this._durationComboBoxFilter.addEventListener(WebInspector.FilterUI.Events.FilterChanged, this._durationFilterChanged, this);
-        filterBar.addFilter(this._durationComboBoxFilter);
 
-        this._categoryFilters = {};
+        this._categoryFiltersUI = {};
         var categoryTypes = [];
         var categories = WebInspector.TimelinePresentationModel.categories();
         for (var categoryName in categories) {
@@ -590,14 +590,18 @@ WebInspector.TimelinePanel.prototype = {
             var filter = new WebInspector.CheckboxFilterUI(category.name, category.title);
             filter.addEventListener(WebInspector.FilterUI.Events.FilterChanged, this._categoriesFilterChanged.bind(this, category.name), this);
             filterBar.addFilter(filter);
-            this._categoryFilters[category.name] = filter;
+            this._categoryFiltersUI[category.name] = filter;
         }
+        this._categoryFilter = new WebInspector.TimelineCategoryFilter();
+
+        this._windowFilter = new WebInspector.TimelineWindowFilter();
+
         return true;
     },
 
     _textFilterChanged: function(event)
     {
-        var searchQuery = this._textFilter.value();
+        var searchQuery = this._textFilterUI.value();
         this._presentationModel.setSearchFilter(null);
         delete this._searchFilter;
 
@@ -617,7 +621,7 @@ WebInspector.TimelinePanel.prototype = {
 
     _durationFilterChanged: function()
     {
-        var duration = this._durationComboBoxFilter.value();
+        var duration = this._durationFilterUI.value();
         var minimumRecordDuration = +duration / 1000.0;
         this._durationFilter.setMinimumRecordDuration(minimumRecordDuration);
         this._invalidateAndScheduleRefresh(true, true);
@@ -626,7 +630,7 @@ WebInspector.TimelinePanel.prototype = {
     _categoriesFilterChanged: function(name, event)
     {
         var categories = WebInspector.TimelinePresentationModel.categories();
-        categories[name].hidden = !this._categoryFilters[name].checked();
+        categories[name].hidden = !this._categoryFiltersUI[name].checked();
         this._invalidateAndScheduleRefresh(true, true);
     },
 
@@ -987,6 +991,7 @@ WebInspector.TimelinePanel.prototype = {
 
     _windowChanged: function()
     {
+        this._windowFilter.setWindowTimes(this._overviewPane.windowStartTime(), this._overviewPane.windowEndTime());
         this._invalidateAndScheduleRefresh(false, true);
         this._selectRecord(null);
     },
@@ -2026,6 +2031,33 @@ WebInspector.TimelineSearchFilter.prototype = {
     accept: function(record)
     {
         return WebInspector.TimelineRecordListRow.testContentMatching(record, this._regExp);
+    }
+}
+
+/**
+ * @constructor
+ * @implements {WebInspector.TimelinePresentationModel.Filter}
+ */
+WebInspector.TimelineWindowFilter = function()
+{
+    this._windowStartTime = 0;
+    this._windowEndTime = Infinity;
+}
+
+WebInspector.TimelineWindowFilter.prototype = {
+    setWindowTimes: function(windowStartTime, windowEndTime)
+    {
+        this._windowStartTime = windowStartTime;
+        this._windowEndTime = windowEndTime;
+    },
+
+    /**
+     * @param {!WebInspector.TimelinePresentationModel.Record} record
+     * @return {boolean}
+     */
+    accept: function(record)
+    {
+        return record.lastChildEndTime >= this._windowStartTime && record.startTime <= this._windowEndTime;
     }
 }
 

Source/devtools/front_end/TimelineOverviewPane.js
@@ -360,24 +360,3 @@ WebInspector.TimelineOverviewBase.prototype = {
 
     __proto__: WebInspector.View.prototype
 }
-
-/**
- * @constructor
- * @implements {WebInspector.TimelinePresentationModel.Filter}
- * @param {!WebInspector.TimelineOverviewPane} pane
- */
-WebInspector.TimelineWindowFilter = function(pane)
-{
-    this._pane = pane;
-}
-
-WebInspector.TimelineWindowFilter.prototype = {
-    /**
-     * @param {!WebInspector.TimelinePresentationModel.Record} record
-     * @return {boolean}
-     */
-    accept: function(record)
-    {
-        return record.lastChildEndTime >= this._pane._windowStartTime && record.startTime <= this._pane._windowEndTime;
-    }
-}

commit 024dc6080d6f0b43f62fd835706f29cf4ce7d164

Timeline: [REGRESSION] windowFilter should be updated when we hit into visible rows limit.
Also we have to reset it when we start new recording session.

It was regressed in r163960

BUG=

Review URL: https://codereview.chromium.org/119083002

git-svn-id: svn://svn.chromium.org/blink/trunk@164165 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/TimelinePanel.js
@@ -835,6 +835,7 @@ WebInspector.TimelinePanel.prototype = {
     _onRecordsCleared: function()
     {
         this._resetPanel();
+        this._windowFilter.reset();
         this._invalidateAndScheduleRefresh(true, true);
     },
 
@@ -1125,7 +1126,9 @@ WebInspector.TimelinePanel.prototype = {
             this._automaticallySizeWindow = false;
             // If we're at the top, always use real timeline start as a left window bound so that expansion arrow padding logic works.
             var windowStartTime = startIndex ? recordsInWindow[startIndex].startTime : this._model.minimumRecordTime();
-            this._overviewPane.setWindowTimes(windowStartTime, recordsInWindow[Math.max(0, lastVisibleLine - 1)].endTime);
+            var windowEndTime = recordsInWindow[Math.max(0, lastVisibleLine - 1)].endTime;
+            this._overviewPane.setWindowTimes(windowStartTime, windowEndTime);
+            this._windowFilter.setWindowTimes(windowStartTime, windowEndTime);
             recordsInWindow = this._presentationModel.filteredRecords();
             endIndex = Math.min(recordsInWindow.length, lastVisibleLine);
         }
@@ -2044,11 +2047,16 @@ WebInspector.TimelineSearchFilter.prototype = {
  */
 WebInspector.TimelineWindowFilter = function()
 {
-    this._windowStartTime = 0;
-    this._windowEndTime = Infinity;
+    this.reset();
 }
 
 WebInspector.TimelineWindowFilter.prototype = {
+    reset: function()
+    {
+        this._windowStartTime = 0;
+        this._windowEndTime = Infinity;
+    },
+
     setWindowTimes: function(windowStartTime, windowEndTime)
     {
         this._windowStartTime = windowStartTime;