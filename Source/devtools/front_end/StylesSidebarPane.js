paulirish/devtools-frontend-2013-2015

commit 07836eb153cf3775b3387e6cd2d2c2f1bc35f651

DevTools: [Styles] Do not bring up color picker for read-only style swatches
R=aandrey, eustas, vsevik
BUG=331056

Review URL: https://codereview.chromium.org/131613003

git-svn-id: svn://svn.chromium.org/blink/trunk@164862 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/StylesSidebarPane.js
@@ -2009,7 +2009,8 @@ WebInspector.StylePropertyTreeElementBase.prototype = {
         var spectrumHelper = this.editablePane() && this.editablePane()._spectrumHelper;
         var spectrum = spectrumHelper ? spectrumHelper.spectrum() : null;
 
-        var colorSwatch = new WebInspector.ColorSwatch();
+        var isEditable = !!(this._styleRule && this._styleRule.editable);
+        var colorSwatch = new WebInspector.ColorSwatch(!isEditable);
         colorSwatch.setColorString(text);
         colorSwatch.element.addEventListener("click", swatchClick.bind(this), false);
 
@@ -2059,28 +2060,32 @@ WebInspector.StylePropertyTreeElementBase.prototype = {
          */
         function swatchClick(e)
         {
+            e.consume(true);
+
             // Shift + click toggles color formats.
             // Click opens colorpicker, only if the element is not in computed styles section.
             if (!spectrumHelper || e.shiftKey) {
                 changeColorDisplay();
-            } else {
-                var visible = spectrumHelper.toggle(colorSwatch.element, color, format);
-
-                if (visible) {
-                    spectrum.displayText = color.toString(format);
-                    this.originalPropertyText = this.property.propertyText;
-                    this.editablePane()._isEditingStyle = true;
-                    spectrum.addEventListener(WebInspector.Spectrum.Events.ColorChanged, boundSpectrumChanged);
-                    spectrumHelper.addEventListener(WebInspector.SpectrumPopupHelper.Events.Hidden, boundSpectrumHidden);
-
-                    scrollerElement = colorSwatch.element.enclosingNodeOrSelfWithClass("scroll-target");
-                    if (scrollerElement)
-                        scrollerElement.addEventListener("scroll", repositionSpectrum, false);
-                    else
-                        console.error("Unable to handle color picker scrolling");
-                }
+                return;
             }
-            e.consume(true);
+
+            if (!isEditable)
+                return;
+
+            var visible = spectrumHelper.toggle(colorSwatch.element, color, format);
+            if (visible) {
+                spectrum.displayText = color.toString(format);
+                this.originalPropertyText = this.property.propertyText;
+                this.editablePane()._isEditingStyle = true;
+                spectrum.addEventListener(WebInspector.Spectrum.Events.ColorChanged, boundSpectrumChanged);
+                spectrumHelper.addEventListener(WebInspector.SpectrumPopupHelper.Events.Hidden, boundSpectrumHidden);
+
+                scrollerElement = colorSwatch.element.enclosingNodeOrSelfWithClass("scroll-target");
+                if (scrollerElement)
+                    scrollerElement.addEventListener("scroll", repositionSpectrum, false);
+                else
+                    console.error("Unable to handle color picker scrolling");
+            }
         }
 
         var colorValueElement = document.createElement("span");

Source/devtools/front_end/Spectrum.js
@@ -427,12 +427,14 @@ WebInspector.SpectrumPopupHelper.prototype = {
 
 /**
  * @constructor
+ * @param {boolean=} readOnly
  */
-WebInspector.ColorSwatch = function()
+WebInspector.ColorSwatch = function(readOnly)
 {
     this.element = document.createElement("span");
     this._swatchInnerElement = this.element.createChild("span", "swatch-inner");
-    this.element.title = WebInspector.UIString("Click to open a colorpicker. Shift-click to change color format");
+    var shiftClickMessage = WebInspector.UIString("Shift-click to change color format.");
+    this.element.title = readOnly ? shiftClickMessage : String.sprintf("%s\n%s", WebInspector.UIString("Click to open a colorpicker."), shiftClickMessage);
     this.element.className = "swatch";
     this.element.addEventListener("mousedown", consumeEvent, false);
     this.element.addEventListener("dblclick", consumeEvent, false);

commit 5bddbae734b47eac6e4ad0218c6732573bcbad89

DevTools: [Styles] Display color swatch for editable styles
This got regressed in r164862.

R=aandrey, eustas, vsevik
NOTRY=true

Review URL: https://codereview.chromium.org/138173004

git-svn-id: svn://svn.chromium.org/blink/trunk@165067 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/StylesSidebarPane.js
@@ -1956,7 +1956,7 @@ WebInspector.StylePropertyTreeElementBase.prototype = {
         var spectrumHelper = this.editablePane() && this.editablePane()._spectrumHelper;
         var spectrum = spectrumHelper ? spectrumHelper.spectrum() : null;
 
-        var isEditable = !!(this._styleRule && this._styleRule.editable);
+        var isEditable = !!(this._styleRule && this._styleRule.editable !== false); // |editable| is true by default.
         var colorSwatch = new WebInspector.ColorSwatch(!isEditable);
         colorSwatch.setColorString(text);
         colorSwatch.element.addEventListener("click", swatchClick.bind(this), false);