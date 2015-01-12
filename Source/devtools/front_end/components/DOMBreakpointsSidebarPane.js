paulirish/devtools-frontend-2013-2015

commit 24be6805883bc93d53c7cd11c4ebf47bee66961d

DevTools: Make labeled checkbox a web component
This does not modify the .checkbox-filter-checkbox checkboxes
to retain the UI.

R=pfeldman

Review URL: https://codereview.chromium.org/805853002

git-svn-id: svn://svn.chromium.org/blink/trunk@187165 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/components/DOMBreakpointsSidebarPane.js
@@ -233,25 +233,23 @@ WebInspector.DOMBreakpointsSidebarPane.prototype = {
         element._type = type;
         element.addEventListener("contextmenu", this._contextMenu.bind(this, node, type), true);
 
-        var checkboxElement = createElement("input");
-        checkboxElement.className = "checkbox-elem";
-        checkboxElement.type = "checkbox";
-        checkboxElement.checked = enabled;
-        checkboxElement.addEventListener("click", this._checkboxClicked.bind(this, node, type), false);
-        element._checkboxElement = checkboxElement;
-        element.appendChild(checkboxElement);
+        var checkboxLabel = createCheckboxLabel(undefined, enabled);
+        checkboxLabel.className = "checkbox-elem";
+        checkboxLabel.checkboxElement.addEventListener("click", this._checkboxClicked.bind(this, node, type), false);
+        element._checkboxElement = checkboxLabel.checkboxElement;
+        element.appendChild(checkboxLabel);
 
-        var labelElement = createElement("span");
-        element.appendChild(labelElement);
 
         var linkifiedNode = WebInspector.DOMPresentationUtils.linkifyNodeReference(node);
         linkifiedNode.classList.add("monospace");
-        labelElement.appendChild(linkifiedNode);
+
+        var textElement = checkboxLabel.createChild("span");
+        textElement.appendChild(linkifiedNode);
 
         var description = createElement("div");
         description.className = "source-text";
         description.textContent = this._breakpointTypeLabels[type];
-        labelElement.appendChild(description);
+        textElement.appendChild(description);
 
         var currentElement = this.listElement.firstChild;
         while (currentElement) {

Source/devtools/devtools.gypi
@@ -212,6 +212,7 @@
             'front_end/toolbox/ResponsiveDesignView.js',
         ],
         'devtools_ui_js_files': [
+            'front_end/ui/checkboxTextLabel.css',
             'front_end/ui/dataGrid.css',
             'front_end/ui/filter.css',
             'front_end/ui/flameChart.css',

Source/devtools/front_end/audits/AuditLauncherView.js
@@ -107,7 +107,7 @@ WebInspector.AuditLauncherView.prototype = {
 
         var selectedCategories = this._selectedCategoriesSetting.get();
         var categoryElement = this._createCategoryElement(category.displayName, category.id);
-        category._checkboxElement = categoryElement.firstChild;
+        category._checkboxElement = categoryElement.checkboxElement;
         if (this._selectAllCheckboxElement.checked || selectedCategories[category.displayName]) {
             category._checkboxElement.checked = true;
             ++this._currentCategoriesCount;
@@ -221,19 +221,15 @@ WebInspector.AuditLauncherView.prototype = {
 
     /**
      * @param {string} title
-     * @param {string} id
+     * @param {string=} id
      */
     _createCategoryElement: function(title, id)
     {
-        var labelElement = createElement("label");
-        labelElement.id = this._categoryIdPrefix + id;
-
-        var element = createElement("input");
-        element.type = "checkbox";
-        if (id !== "")
-            element.addEventListener("click", this._boundCategoryClickListener, false);
-        labelElement.appendChild(element);
-        labelElement.createTextChild(title);
+        var labelElement = createCheckboxLabel(title);
+        if (id) {
+            labelElement.id = this._categoryIdPrefix + id;
+            labelElement.checkboxElement.addEventListener("click", this._boundCategoryClickListener, false);
+        }
         labelElement.__displayName = title;
 
         return labelElement;
@@ -259,7 +255,7 @@ WebInspector.AuditLauncherView.prototype = {
         }
         var categoryElement = this._createCategoryElement(WebInspector.UIString("Select All"), "");
         categoryElement.id = "audit-launcher-selectall";
-        this._selectAllCheckboxElement = categoryElement.firstChild;
+        this._selectAllCheckboxElement = categoryElement.checkboxElement;
         this._selectAllCheckboxElement.checked = this._selectedCategoriesSetting.get()[WebInspector.AuditLauncherView.AllCategoriesKey];
         this._selectAllCheckboxElement.addEventListener("click", handleSelectAllClick.bind(this), false);
         this._contentElement.appendChild(categoryElement);

Source/devtools/front_end/components/breakpointsList.css
@@ -72,19 +72,16 @@ ol.breakpoint-list {
 }
 
 .breakpoint-list .checkbox-elem {
-    font-size: 10px;
     margin: 0 4px;
     vertical-align: top;
     position: relative;
     z-index: 1;
-    top: 3px;
 }
 
 .breakpoint-list .source-text {
     white-space: nowrap;
     text-overflow: ellipsis;
     overflow: hidden;
-    margin: 2px 0 0 20px;
 }
 
 .sidebar-pane .breakpoint-hit {
@@ -112,8 +109,5 @@ li.breakpoint-hit .breakpoint-hit-marker {
 }
 
 .event-listener-breakpoints .checkbox-elem {
-    float: left;
-    position: relative;
-    top: 1px;
     font-size: 10px;
 }

Source/devtools/front_end/elements/ElementsPanel.js
@@ -720,7 +720,7 @@ WebInspector.ElementsPanel.prototype = {
      */
     handleCopyEvent: function(event)
     {
-        if (!WebInspector.currentFocusElement().enclosingNodeOrSelfWithClass("elements-tree-outline"))
+        if (!WebInspector.currentFocusElement() || !WebInspector.currentFocusElement().enclosingNodeOrSelfWithClass("elements-tree-outline"))
             return;
         var treeOutline = this._treeOutlineForNode(this.selectedDOMNode());
         if (treeOutline)

Source/devtools/front_end/elements/StylesSidebarPane.js
@@ -949,14 +949,11 @@ WebInspector.StylesSidebarPane.prototype = {
         function createCheckbox(state)
         {
             var td = createElement("td");
-            var label = createElement("label");
-            var input = createElement("input");
-            input.type = "checkbox";
+            var label = createCheckboxLabel(":" + state);
+            var input = label.checkboxElement;
             input.state = state;
             input.addEventListener("click", clickListener.bind(this), false);
             inputs.push(input);
-            label.appendChild(input);
-            label.createTextChild(":" + state);
             td.appendChild(label);
             return td;
         }

Source/devtools/front_end/elements/elementsPanel.css
@@ -715,8 +715,11 @@ li.child-editing .styles-clipboard-only {
 }
 
 label.checkbox-with-label {
-    display: flex;
     -webkit-user-select: none;
+}
+
+label.checkbox-with-label > .dt-checkbox-text {
+    margin-top: 1px;
 }
 
 .events-pane .section:not(:first-of-type) {

Source/devtools/front_end/inspectorCommon.css
@@ -145,10 +145,3 @@ body /deep/ input[type="search"]:focus,
 body /deep/ input[type="text"]:focus {
     outline: auto 5px -webkit-focus-ring-color;
 }
-
-body /deep/ input[type="checkbox"] {
-    height: 12px;
-    width: 12px;
-    margin: auto 3px;
-    flex-shrink: 0;
-}

commit 1c000e0691c91c804b92b64d7440e8f3e77c45f8

DevTools: Fix DOM breakpoints pane UI regression from r187165
R=vsevik
BUG=446117

Review URL: https://codereview.chromium.org/830373003

git-svn-id: svn://svn.chromium.org/blink/trunk@188210 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/components/DOMBreakpointsSidebarPane.js
@@ -233,23 +233,25 @@ WebInspector.DOMBreakpointsSidebarPane.prototype = {
         element._type = type;
         element.addEventListener("contextmenu", this._contextMenu.bind(this, node, type), true);
 
-        var checkboxLabel = createCheckboxLabel(undefined, enabled);
-        checkboxLabel.className = "checkbox-elem";
-        checkboxLabel.checkboxElement.addEventListener("click", this._checkboxClicked.bind(this, node, type), false);
-        element._checkboxElement = checkboxLabel.checkboxElement;
-        element.appendChild(checkboxLabel);
+        var checkboxElement = createElement("input");
+        checkboxElement.className = "checkbox-elem";
+        checkboxElement.type = "checkbox";
+        checkboxElement.checked = enabled;
+        checkboxElement.addEventListener("click", this._checkboxClicked.bind(this, node, type), false);
+        element._checkboxElement = checkboxElement;
+        element.appendChild(checkboxElement);
 
+        var labelElement = createElement("span");
+        element.appendChild(labelElement);
 
         var linkifiedNode = WebInspector.DOMPresentationUtils.linkifyNodeReference(node);
         linkifiedNode.classList.add("monospace");
-
-        var textElement = checkboxLabel.createChild("span");
-        textElement.appendChild(linkifiedNode);
+        labelElement.appendChild(linkifiedNode);
 
         var description = createElement("div");
         description.className = "source-text";
         description.textContent = this._breakpointTypeLabels[type];
-        textElement.appendChild(description);
+        labelElement.appendChild(description);
 
         var currentElement = this.listElement.firstChild;
         while (currentElement) {