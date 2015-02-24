paulirish/devtools-frontend-2013-2015

commit b12830fc2ab9ea6cb4167cbd4c66bc7382a87532

DevTools: Extract WI.PropertiesSection from WI.Section and fix their misuses.
R=pfeldman

Review URL: https://codereview.chromium.org/854533003

git-svn-id: svn://svn.chromium.org/blink/trunk@188468 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/elements/StylesSidebarPane.js
@@ -746,7 +746,7 @@ WebInspector.StylesSidebarPane.prototype = {
         this._nodeStylesUpdatedForTest(node, false);
 
         /**
-         * @param {!WebInspector.Section} section
+         * @param {!WebInspector.StylePropertiesSection} section
          * @return {boolean}
          */
         function nonBlankSections(section)
@@ -933,7 +933,7 @@ WebInspector.StylesSidebarPane.prototype = {
 
             var section = new WebInspector.StylePropertiesSection(this, sectionModel);
             section._markSelectorMatches();
-            section.expand();
+            section.onpopulate();
             this._sectionsContainer.appendChild(section.element);
             sections.push(section);
         }
@@ -1423,23 +1423,25 @@ WebInspector.ComputedStyleSidebarPane.prototype = {
 
 /**
  * @constructor
- * @extends {WebInspector.Section}
  * @param {!WebInspector.StylesSidebarPane} parentPane
  * @param {!WebInspector.StylesSectionModel} styleRule
  */
 WebInspector.StylePropertiesSection = function(parentPane, styleRule)
 {
-    WebInspector.Section.call(this, "");
-
     this._parentPane = parentPane;
     this.styleRule = styleRule;
     this.editable = styleRule.editable();
 
     var rule = styleRule.rule();
     var extraClasses = (rule && (rule.isInjected || rule.isUserAgent) ? " user-rule" : "");
-    this.element.className = "styles-section matched-styles monospace" + extraClasses;
-    // We don't really use properties' disclosure.
-    this.propertiesElement.classList.remove("properties-tree");
+    this.element = createElementWithClass("div", "styles-section matched-styles monospace" + extraClasses);
+    this.element._section = this;
+
+    this.titleElement = this.element.createChild("div", "styles-section-title " + (rule ? "styles-selector" : ""));
+    this.propertiesElement = this.element.createChild("ol", "style-properties monospace");
+    this.propertiesTreeOutline = new TreeOutline(this.propertiesElement, true);
+    this.propertiesTreeOutline.setFocusable(false);
+    this.propertiesTreeOutline.section = this;
 
     var selectorContainer = createElement("div");
     this._selectorElement = createElement("span");
@@ -1468,18 +1470,16 @@ WebInspector.StylePropertiesSection = function(parentPane, styleRule)
 
     if (rule) {
         // Prevent editing the user agent and user rules.
-        if (rule.isUserAgent || rule.isInjected)
+        if (rule.isUserAgent || rule.isInjected) {
             this.editable = false;
-        else {
+        } else {
             // Check this is a real CSSRule, not a bogus object coming from WebInspector.BlankStylePropertiesSection.
             if (rule.styleSheetId)
                 this.navigable = !!rule.resourceURL();
         }
-        this.titleElement.classList.add("styles-selector");
     }
 
-    this._selectorRefElement = createElement("div");
-    this._selectorRefElement.className = "subtitle";
+    this._selectorRefElement = createElementWithClass("div", "styles-section-subtitle");
     this._mediaListElement = this.titleElement.createChild("div", "media-list media-matches");
     this._updateMediaList();
     this._updateRuleOrigin();
@@ -1665,22 +1665,6 @@ WebInspector.StylePropertiesSection.prototype = {
     },
 
     /**
-     * @override
-     */
-    collapse: function()
-    {
-        // Overriding with empty body.
-    },
-
-    /**
-     * @override
-     */
-    handleClick: function()
-    {
-        // Avoid consuming events.
-    },
-
-    /**
      * @param {string} propertyName
      * @return {boolean}
      */
@@ -1742,7 +1726,7 @@ WebInspector.StylePropertiesSection.prototype = {
         this._markSelectorMatches();
         if (full) {
             this.propertiesTreeOutline.removeChildren();
-            this.repopulate();
+            this.onpopulate();
         } else {
             var child = this.propertiesTreeOutline.children[0];
             while (child) {
@@ -1958,7 +1942,6 @@ WebInspector.StylePropertiesSection.prototype = {
             event.consume();
             return;
         }
-        this.expand();
         this.addNewBlankProperty().startEditing();
         event.consume(true);
     },
@@ -2085,7 +2068,6 @@ WebInspector.StylePropertiesSection.prototype = {
             return;
 
         if (!this.rule() && this.propertiesTreeOutline.children.length === 0) {
-            this.expand();
             this.addNewBlankProperty().startEditing();
             return;
         }
@@ -2149,7 +2131,6 @@ WebInspector.StylePropertiesSection.prototype = {
             return;
 
         if (moveDirection === "forward") {
-            this.expand();
             var firstChild = this.propertiesTreeOutline.children[0];
             while (firstChild && firstChild.inherited)
                 firstChild = firstChild.nextSibling;
@@ -2162,7 +2143,6 @@ WebInspector.StylePropertiesSection.prototype = {
             if (!previousSection)
                 return;
 
-            previousSection.expand();
             previousSection.addNewBlankProperty().startEditing();
         }
     },
@@ -2242,9 +2222,7 @@ WebInspector.StylePropertiesSection.prototype = {
         // Mark the selectors in group if necessary.
         // This is overridden by BlankStylePropertiesSection.
         this._markSelectorMatches();
-    },
-
-    __proto__: WebInspector.Section.prototype
+    }
 }
 
 /**
@@ -2300,7 +2278,7 @@ WebInspector.StylePropertiesSection._linkifyRuleLocation = function(target, link
 
 /**
  * @constructor
- * @extends {WebInspector.Section}
+ * @extends {WebInspector.PropertiesSection}
  * @param {!WebInspector.ComputedStyleSidebarPane} stylesPane
  * @param {!WebInspector.StylesSectionModel} styleRule
  * @param {!WebInspector.SectionCascade} matchedRuleCascade
@@ -2308,7 +2286,7 @@ WebInspector.StylePropertiesSection._linkifyRuleLocation = function(target, link
  */
 WebInspector.ComputedStylePropertiesSection = function(stylesPane, styleRule, matchedRuleCascade, animationProperties)
 {
-    WebInspector.Section.call(this, "");
+    WebInspector.PropertiesSection.call(this, "");
     this.element.className = "styles-section monospace read-only computed-style";
 
     this.headerElement.appendChild(WebInspector.ComputedStylePropertiesSection._showInheritedCheckbox());
@@ -2458,7 +2436,7 @@ WebInspector.ComputedStylePropertiesSection.prototype = {
         }
     },
 
-    __proto__: WebInspector.Section.prototype
+    __proto__: WebInspector.PropertiesSection.prototype
 }
 
 /**
@@ -2472,7 +2450,6 @@ WebInspector.ComputedStylePropertiesSection.prototype = {
  */
 WebInspector.BlankStylePropertiesSection = function(stylesPane, defaultSelectorText, styleSheetId, ruleLocation, insertAfterStyleRule)
 {
-    var styleSheetHeader = WebInspector.cssModel.styleSheetHeaderForId(styleSheetId);
     var dummyCascade = new WebInspector.SectionCascade();
     var blankSectionModel = dummyCascade.appendModelFromStyle(WebInspector.CSSStyleDeclaration.createDummyStyle(), defaultSelectorText);
     blankSectionModel.setEditable(true);
@@ -2517,15 +2494,6 @@ WebInspector.BlankStylePropertiesSection.prototype = {
 
     /**
      * @override
-     */
-    expand: function()
-    {
-        if (!this.isBlank)
-            WebInspector.StylePropertiesSection.prototype.expand.call(this);
-    },
-
-    /**
-     * @override
      * @param {!Element} element
      * @param {string} newContent
      * @param {string} oldContent
@@ -2558,7 +2526,6 @@ WebInspector.BlankStylePropertiesSection.prototype = {
             this._parentPane._styleSheetRuleEdited(newRule, this._ruleLocation, newRange);
 
             this._updateRuleOrigin();
-            this.expand();
             if (this.element.parentElement) // Might have been detached already.
                 this._moveEditorFromSelector(moveDirection);
 
@@ -3220,8 +3187,6 @@ WebInspector.StylePropertyTreeElement.prototype = {
             if (!newStyle)
                 return;
             this._applyNewStyle(newStyle);
-
-            var section = this.section();
 
             this._updatePane();
             this.styleTextAppliedForTest();

Source/devtools/devtools.gypi
@@ -222,8 +222,10 @@
             'front_end/ui/panelEnablerView.css',
             'front_end/ui/pieChart.css',
             'front_end/ui/progressIndicator.css',
+            'front_end/ui/propertiesSection.css',
             'front_end/ui/radioButton.css',
             'front_end/ui/searchableView.css',
+            'front_end/ui/section.css',
             'front_end/ui/softContextMenu.css',
             'front_end/ui/splitView.css',
             'front_end/ui/statusBar.css',

Source/devtools/front_end/components/ObjectPropertiesSection.js
@@ -26,7 +26,7 @@
 
 /**
  * @constructor
- * @extends {WebInspector.Section}
+ * @extends {WebInspector.PropertiesSection}
  * @param {!WebInspector.RemoteObject} object
  * @param {?string|!Element=} title
  * @param {string=} subtitle
@@ -45,7 +45,7 @@ WebInspector.ObjectPropertiesSection = function(object, title, subtitle, emptyPl
     this.editable = true;
     this.skipProto = false;
 
-    WebInspector.Section.call(this, title || "", subtitle);
+    WebInspector.PropertiesSection.call(this, title || "", subtitle);
 }
 
 /** @const */
@@ -115,7 +115,7 @@ WebInspector.ObjectPropertiesSection.prototype = {
         this.propertiesForTest = properties;
     },
 
-    __proto__: WebInspector.Section.prototype
+    __proto__: WebInspector.PropertiesSection.prototype
 }
 
 /**

Source/devtools/front_end/elements/AnimationsSidebarPane.js
@@ -387,7 +387,7 @@ WebInspector.AnimationSection.prototype = {
                 model.setIsAttribute(true);
                 model.setEditable(true);
                 var styleSection = new WebInspector.StylePropertiesSection(this._stylesPane, model);
-                styleSection.expand();
+                styleSection.onpopulate();
                 this._keyframesElement.appendChild(styleSection.element);
             }
         }

Source/devtools/front_end/elements/EventListenersSidebarPane.js
@@ -160,11 +160,6 @@ WebInspector.EventListenersSection = function(title, nodeId, linkifier)
     this._linkifier = linkifier;
     WebInspector.Section.call(this, title);
 
-    // Changed from a Properties List
-    this.propertiesElement.remove();
-    delete this.propertiesElement;
-    delete this.propertiesTreeOutline;
-
     this._eventBars = this.element.createChild("div", "event-bars");
 }
 

Source/devtools/front_end/elements/elementsPanel.css
@@ -178,24 +178,24 @@
     background-color: #eee;
 }
 
-.styles-section .properties li.not-parsed-ok {
+.styles-section .style-properties li.not-parsed-ok {
     margin-left: 0;
 }
 
-.styles-section.computed-style .properties li.not-parsed-ok {
+.styles-section.computed-style .style-properties li.not-parsed-ok {
     margin-left: -6px;
 }
 
-.styles-section .properties li.filter-match,
+.styles-section .style-properties li.filter-match,
 .styles-section .simple-selector.filter-match {
     background-color: rgba(255, 255, 0, 0.5);
 }
 
-.styles-section .properties li.overloaded.filter-match {
+.styles-section .style-properties li.overloaded.filter-match {
     background-color: rgba(255, 255, 0, 0.25);
 }
 
-.styles-section .properties li.not-parsed-ok .exclamation-mark {
+.styles-section .style-properties li.not-parsed-ok .exclamation-mark {
     display: inline-block;
     position: relative;
     width: 11px;
@@ -208,31 +208,27 @@
     z-index: 1;
 }
 
-.styles-section .header {
-    white-space: nowrap;
+.styles-section-title {
     background-origin: padding;
     background-clip: padding;
-}
-
-.styles-section .header .title {
     word-wrap: break-word;
     white-space: normal;
 }
 
-.styles-section .header .title .media-list {
+.styles-section-title .media-list {
     color: #888;
 }
 
-.styles-section .header .title .media-list.media-matches .media.editable-media {
+.styles-section-title .media-list.media-matches .media.editable-media {
     color: #222;
 }
 
-.styles-section .header .title .media:not(.editing-media),
-.styles-section .header .title .media:not(.editing-media) .subtitle {
+.styles-section-title .media:not(.editing-media),
+.styles-section-title .media:not(.editing-media) .subtitle {
     overflow: hidden;
 }
 
-.styles-section .header .subtitle {
+.styles-section-subtitle {
     color: rgb(85, 85, 85);
     float: right;
     margin-left: 5px;
@@ -242,7 +238,7 @@
     white-space: nowrap;
 }
 
-.styles-section .header .subtitle a {
+.styles-section .styles-section-subtitle a {
     color: inherit;
 }
 
@@ -263,31 +259,26 @@
     text-decoration: underline;
 }
 
-.styles-section .properties {
-    display: none;
+.styles-section .style-properties {
     margin: 0;
     padding: 2px 4px 0 0;
     list-style: none;
     clear: both;
 }
 
-.styles-section.matched-styles .properties {
+.styles-section.matched-styles .style-properties {
     padding-left: 0;
 }
 
-.styles-section.no-affect .properties li {
+.styles-section.no-affect .style-properties li {
     opacity: 0.5;
 }
 
-.styles-section.no-affect .properties li.editing {
+.styles-section.no-affect .style-properties li.editing {
     opacity: 1.0;
 }
 
-.styles-section.expanded .properties {
-    display: block;
-}
-
-.styles-section .properties li {
+.styles-section .style-properties li {
     margin-left: 12px;
     padding-left: 22px;
     white-space: normal;
@@ -296,56 +287,56 @@
     cursor: auto;
 }
 
-.styles-section.computed-style.expanded .properties > li {
+.styles-section.computed-style .style-properties > li {
     padding-left: 0;
 }
 
-.styles-section.computed-style.expanded .properties > li .webkit-css-property {
+.styles-section.computed-style .style-properties > li .webkit-css-property {
     margin-left: 0;
 }
 
-.styles-section .properties li .webkit-css-property {
-    margin-left: -22px; /* outdent the first line of longhand properties (in an expanded shorthand) to compensate for the "padding-left" shift in .styles-section .properties li */
+.styles-section .style-properties li .webkit-css-property {
+    margin-left: -22px; /* outdent the first line of longhand properties (in an expanded shorthand) to compensate for the "padding-left" shift in .styles-section .style-properties li */
 }
 
-.styles-section.expanded .properties > li {
+.styles-section .style-properties > li {
     padding-left: 38px;
 }
 
-.styles-section .properties > li .webkit-css-property {
-    margin-left: -38px; /* outdent the first line of the top-level properties to compensate for the "padding-left" shift in .styles-section .properties > li */
+.styles-section .style-properties > li .webkit-css-property {
+    margin-left: -38px; /* outdent the first line of the top-level properties to compensate for the "padding-left" shift in .styles-section .style-properties > li */
 }
 
-.styles-section .properties > li.child-editing {
+.styles-section .style-properties > li.child-editing {
     padding-left: 8px;
 }
 
-.styles-section .properties > li.child-editing .webkit-css-property {
+.styles-section .style-properties > li.child-editing .webkit-css-property {
     margin-left: 0;
 }
 
-.styles-section.matched-styles .properties li {
+.styles-section.matched-styles .style-properties li {
     margin-left: 0 !important;
 }
 
-.styles-section .properties li.child-editing {
+.styles-section .style-properties li.child-editing {
     word-wrap: break-word !important;
     white-space: normal !important;
     padding-left: 0;
 }
 
-.styles-section .properties ol {
+.styles-section .style-properties ol {
     display: none;
     margin: 0;
     -webkit-padding-start: 12px;
     list-style: none;
 }
 
-.styles-section .properties ol.expanded {
+.styles-section .style-properties ol.expanded {
     display: block;
 }
 
-.styles-section.matched-styles .properties li.parent .expand-element {
+.styles-section.matched-styles .style-properties li.parent .expand-element {
     -webkit-user-select: none;
     background-image: url(Images/statusbarButtonGlyphs.png);
     background-size: 320px 144px;
@@ -358,33 +349,33 @@
 }
 
 @media (-webkit-min-device-pixel-ratio: 1.5) {
-.styles-section.matched-styles .properties li.parent .expand-element {
+.styles-section.matched-styles .style-properties li.parent .expand-element {
     background-image: url(Images/statusbarButtonGlyphs_2x.png);
 }
 } /* media */
 
-.styles-section.matched-styles .properties li.parent .expand-element {
+.styles-section.matched-styles .style-properties li.parent .expand-element {
     background-position: -4px -96px;
 }
 
-.styles-section.matched-styles .properties li.parent.expanded .expand-element {
+.styles-section.matched-styles .style-properties li.parent.expanded .expand-element {
     background-position: -20px -96px;
 }
 
-.styles-section .properties li .info {
+.styles-section .style-properties li .info {
     padding-top: 4px;
     padding-bottom: 3px;
 }
 
-.styles-section.matched-styles:not(.read-only):hover .properties .enabled-button {
+.styles-section.matched-styles:not(.read-only):hover .style-properties .enabled-button {
     visibility: visible;
 }
 
-.styles-section.matched-styles:not(.read-only) .properties li.disabled .enabled-button {
+.styles-section.matched-styles:not(.read-only) .style-properties li.disabled .enabled-button {
     visibility: visible;
 }
 
-.styles-section .properties .enabled-button {
+.styles-section .style-properties .enabled-button {
     visibility: hidden;
     float: left;
     font-size: 10px;
@@ -397,32 +388,32 @@
     top: 1px;
 }
 
-.styles-section.matched-styles .properties ol.expanded {
+.styles-section.matched-styles .style-properties ol.expanded {
     margin-left: 16px;
 }
 
-.styles-section .properties .overloaded:not(.has-ignorable-error),
-.styles-section .properties .inactive,
-.styles-section .properties .disabled,
-.styles-section .properties .not-parsed-ok:not(.has-ignorable-error) {
+.styles-section .style-properties .overloaded:not(.has-ignorable-error),
+.styles-section .style-properties .inactive,
+.styles-section .style-properties .disabled,
+.styles-section .style-properties .not-parsed-ok:not(.has-ignorable-error) {
     text-decoration: line-through;
 }
 
-.styles-section .properties .has-ignorable-error .webkit-css-property {
+.styles-section .style-properties .has-ignorable-error .webkit-css-property {
     color: inherit;
 }
 
-.styles-section.computed-style .properties .disabled {
+.styles-section.computed-style .style-properties .disabled {
     text-decoration: none;
     opacity: 0.5;
 }
 
-.styles-section .properties .implicit,
-.styles-section .properties .inherited {
+.styles-section .style-properties .implicit,
+.styles-section .style-properties .inherited {
     opacity: 0.5;
 }
 
-.styles-section .properties .has-ignorable-error {
+.styles-section .style-properties .has-ignorable-error {
     color: gray;
 }
 
@@ -689,9 +680,9 @@
     color: black;
 }
 
-.styles-section:not(.read-only) .properties .webkit-css-property.styles-panel-hovered,
-.styles-section:not(.read-only) .properties .value .styles-panel-hovered,
-.styles-section:not(.read-only) .properties .value.styles-panel-hovered,
+.styles-section:not(.read-only) .style-properties .webkit-css-property.styles-panel-hovered,
+.styles-section:not(.read-only) .style-properties .value .styles-panel-hovered,
+.styles-section:not(.read-only) .style-properties .value.styles-panel-hovered,
 .styles-section:not(.read-only) span.simple-selector.styles-panel-hovered {
     text-decoration: underline;
     cursor: default;
@@ -783,4 +774,15 @@ label.checkbox-with-label {
 
 .event-bar .event-properties {
     padding-left: 16px;
+}
+
+.style-properties li.editing {
+    margin-left: 10px;
+    text-overflow: clip;
+}
+
+.style-properties li.editing-sub-part {
+    padding: 3px 6px 8px 18px;
+    margin: -1px -6px -8px -6px;
+    text-overflow: clip;
 }

Source/devtools/front_end/inspectorStyle.css
@@ -252,12 +252,12 @@ body.undocked.platform-mac.inactive .inspector-view-tabbed-pane.tabbed-pane::sha
     margin-top: 0;
 }
 
-.console-formatted-object .properties,
-.console-formatted-map .properties,
-.console-formatted-set .properties,
-.console-formatted-iterator .properties,
-.console-formatted-generator .properties,
-.console-formatted-node .properties {
+.console-formatted-object .properties-tree,
+.console-formatted-map .properties-tree,
+.console-formatted-set .properties-tree,
+.console-formatted-iterator .properties-tree,
+.console-formatted-generator .properties-tree,
+.console-formatted-node .properties-tree {
     padding-left: 0 !important;
 }
 
@@ -525,153 +525,12 @@ iframe.panel.extension {
     color: #999;
 }
 
-.section {
-    position: relative;
-    margin-top: 1px;
-}
-
-.section > .header {
-    padding: 0 8px 0 5px;
-    min-height: 18px;
-    white-space: nowrap;
-    background-origin: padding-box;
-    background-clip: padding-box;
-}
-
-.section > .header::before {
-    -webkit-user-select: none;
-    background-image: url(Images/statusbarButtonGlyphs.png);
-    background-size: 320px 144px;
-    opacity: 0.5;
-    content: "a";
-    color: transparent;
-    text-shadow: none;
-    float: left;
-    width: 8px;
-    margin-right: 4px;
-    margin-top: 2px;
-}
-
-@media (-webkit-min-device-pixel-ratio: 1.5) {
-.section > .header::before {
-    background-image: url(Images/statusbarButtonGlyphs_2x.png);
-}
-} /* media */
-
-.section > .header::before {
-    background-position: -4px -96px;
-}
-
-.section.expanded > .header::before {
-    background-position: -20px -96px;
-}
-
-.section > .header .title {
-    font-weight: normal;
-    word-wrap: break-word;
-    white-space: normal;
-    line-height: 18px;
-}
-
-.section > .header label {
-    display: none;
-}
-
-.section.expanded .header label {
-    display: inline;
-}
-
-.section > .header .subtitle {
-    float: right;
-    margin-left: 5px;
-    max-width: 55%;
-    text-overflow: ellipsis;
-    overflow: hidden;
-}
-
-.section > .header .subtitle a {
-    color: inherit;
-}
-
-.section > .properties {
-    display: none;
-}
-
-.section.expanded > .properties {
-    display: block;
-}
-
-.properties-tree {
-    margin: 0;
-    padding: 0 6px 2px;
-    list-style: none;
-    min-height: 18px;
-}
-
-.properties-tree li {
-    margin-left: 12px;
-    white-space: nowrap;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    -webkit-user-select: text;
-    cursor: default;
-    padding-top: 2px;
-    line-height: 12px;
-}
-
-.properties-tree li.parent {
-    margin-left: 1px;
-}
-
-.properties-tree li.parent::before {
-    -webkit-user-select: none;
-    background-image: url(Images/statusbarButtonGlyphs.png);
-    background-size: 320px 144px;
-    opacity: 0.5;
-    content: "a";
-    width: 8px;
-    float: left;
-    margin-right: 4px;
-    color: transparent;
-    text-shadow: none;
-}
-
 .properties-calculate-value-button:hover {
     text-decoration: underline;
 }
 
 .properties-accessor-property-name {
     font-style: italic;
-}
-
-@media (-webkit-min-device-pixel-ratio: 1.5) {
-.properties-tree li.parent::before {
-    background-image: url(Images/statusbarButtonGlyphs_2x.png);
-}
-} /* media */
-
-.properties-tree li.parent::before {
-    background-position: -4px -96px;
-}
-
-.properties-tree li.parent.expanded::before {
-    background-position: -20px -96px;
-}
-
-.properties-tree li .info {
-    padding-top: 4px;
-    padding-bottom: 3px;
-}
-
-.properties-tree ol {
-    display: none;
-    margin: 0;
-    -webkit-padding-start: 12px;
-    list-style: none;
-}
-
-.properties-tree ol.expanded {
-    display: block;
 }
 
 .editing {
@@ -704,46 +563,6 @@ iframe.panel.extension {
 
 .editing br {
     display: none;
-}
-
-.section .properties li.editing {
-    margin-left: 10px;
-    text-overflow: clip;
-}
-
-.section .properties li.editing-sub-part {
-    padding: 3px 6px 8px 18px;
-    margin: -1px -6px -8px -6px;
-    text-overflow: clip;
-}
-
-.section .properties .name {
-    color: rgb(136, 19, 145);
-    flex-shrink: 0;
-}
-
-.section .properties .separator {
-    flex-shrink: 0;
-}
-
-.section .properties .dimmed {
-    opacity: 0.6;
-}
-
-.section .properties .value.error {
-    color: red;
-}
-
-.section .properties .number {
-    color: blue;
-}
-
-.section .properties .keyword {
-    color: rgb(136, 19, 79);
-}
-
-.section .properties .color {
-    color: rgb(118, 15, 21);
 }
 
 .swatch {

Source/devtools/front_end/sidebarPane.css
@@ -58,7 +58,7 @@
     display: block;
 }
 
-.sidebar-pane .section .properties {
+.sidebar-pane .section .properties-tree {
     padding-left: 16px;
 }
 

commit 0dff4d54f15b67c5766bf98dbfc3175915c560b3

DevTools: [SSP] fade out inherited properties in Computed Properties
This patch fixes a regression after r188468 which missed properly
styling for ComputedStylePropertySection elements.

BUG=459997
R=pfeldman

Review URL: https://codereview.chromium.org/951873003

git-svn-id: svn://svn.chromium.org/blink/trunk@190752 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/elements/StylesSidebarPane.js
@@ -2281,6 +2281,7 @@ WebInspector.ComputedStylePropertiesSection = function(stylesPane, styleRule, ma
 {
     WebInspector.PropertiesSection.call(this, "");
     this.element.className = "styles-section monospace read-only computed-style";
+    this.propertiesElement.classList.add("style-properties");
 
     this.headerElement.appendChild(WebInspector.ComputedStylePropertiesSection._showInheritedCheckbox());
 