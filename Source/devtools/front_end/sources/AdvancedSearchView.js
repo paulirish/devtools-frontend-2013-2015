paulirish/devtools-frontend-2013-2015

commit 410d8b04c8de85b88afaa251598ba516bcc24d53

DevTools: make Search in sources closeable, initiate from top level menu.
BUG=512672
NOTRY=true

Review URL: https://codereview.chromium.org/1250313002

git-svn-id: svn://svn.chromium.org/blink/trunk@199533 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/sources/AdvancedSearchView.js
@@ -50,13 +50,8 @@ WebInspector.AdvancedSearchView = function()
 
     this._advancedSearchConfig = WebInspector.settings.createLocalSetting("advancedSearchConfig", new WebInspector.SearchConfig("", true, false).toPlainObject());
     this._load();
-    WebInspector.AdvancedSearchView._instance = this;
     /** @type {!WebInspector.SearchScope} */
     this._searchScope = new WebInspector.SourcesSearchScope();
-    if (WebInspector.AdvancedSearchView._pendingQuery !== undefined) {
-        this._toggle(WebInspector.AdvancedSearchView._pendingQuery);
-        delete WebInspector.AdvancedSearchView._pendingQuery;
-    }
 }
 
 WebInspector.AdvancedSearchView.prototype = {
@@ -346,6 +341,7 @@ WebInspector.SearchResultsPane.prototype = {
  */
 WebInspector.AdvancedSearchView.ActionDelegate = function()
 {
+    this._searchView = new WebInspector.AdvancedSearchView();
 }
 
 WebInspector.AdvancedSearchView.ActionDelegate.prototype = {
@@ -356,21 +352,15 @@ WebInspector.AdvancedSearchView.ActionDelegate.prototype = {
      */
     handleAction: function(context, actionId)
     {
-        var searchView = WebInspector.AdvancedSearchView._instance;
-        if (!searchView || !searchView.isShowing() || searchView._search !== searchView.element.window().document.activeElement) {
+        if (!this._searchView.isShowing() || this._searchView._search !== this._searchView.element.window().document.activeElement) {
             var selection = WebInspector.inspectorView.element.getDeepSelection();
             var queryCandidate = "";
             if (selection.rangeCount)
                 queryCandidate = selection.toString().replace(/\r?\n.*/, "");
 
             WebInspector.inspectorView.setCurrentPanel(WebInspector.SourcesPanel.instance());
-            WebInspector.inspectorView.showViewInDrawer("sources.search");
-            if (WebInspector.AdvancedSearchView._instance)
-                WebInspector.AdvancedSearchView._instance._toggle(queryCandidate);
-            else
-                WebInspector.AdvancedSearchView._pendingQuery = queryCandidate;
-        } else {
-            WebInspector.inspectorView.closeDrawer();
+            this._searchView._toggle(queryCandidate);
+            WebInspector.inspectorView.showCloseableViewInDrawer("sources.search", WebInspector.UIString("Search"), this._searchView);
         }
     }
 }

Source/devtools/front_end/ui/TabbedPane.js
@@ -340,8 +340,11 @@ WebInspector.TabbedPane.prototype = {
         var tab = this._tabsById[id];
         if (!tab)
             return false;
-        if (this._currentTab && this._currentTab.id === id)
+        if (this._currentTab && this._currentTab.id === id) {
+            if (userGesture)
+                this.focus();
             return true;
+        }
 
         this._hideCurrentTab();
         this._showTab(tab);
@@ -351,7 +354,7 @@ WebInspector.TabbedPane.prototype = {
         this._tabsHistory.splice(0, 0, tab);
 
         this._updateTabElements();
-        if (focused)
+        if (focused || userGesture)
             this.focus();
 
         var eventData = { tabId: id, view: tab.view, isUserGesture: userGesture };
@@ -1336,6 +1339,8 @@ WebInspector.ExtensibleTabbedPaneController.prototype = {
      */
     _tabOrderComparator: function(id1, id2)
     {
-        return this._tabOrders[id2] = this._tabOrders[id1];
+        var weight1 = id1 in this._tabOrders ? this._tabOrders[id1] : 1000000;
+        var weight2 = id2 in this._tabOrders ? this._tabOrders[id2] : 1000000;
+        return weight1 - weight2;
     }
 }

Source/devtools/front_end/Runtime.js
@@ -845,7 +845,9 @@ Runtime.Extension.prototype = {
     enabled: function()
     {
         var activatorExperiment = this.descriptor()["experiment"];
-        if (activatorExperiment && !Runtime.experiments.isEnabled(activatorExperiment))
+        if (activatorExperiment && activatorExperiment.startsWith("!") && Runtime.experiments.isEnabled(activatorExperiment.substring(1)))
+            return false;
+        if (activatorExperiment && !activatorExperiment.startsWith("!") && !Runtime.experiments.isEnabled(activatorExperiment))
             return false;
         var condition = this.descriptor()["condition"];
         if (condition && !Runtime.queryParam(condition))

Source/devtools/front_end/components/Drawer.js
@@ -104,6 +104,7 @@ WebInspector.Drawer.prototype = {
         }
         this._innerShow();
         this._tabbedPane.selectTab(id, true);
+        this._lastSelectedViewSetting.set(id);
     },
 
     showDrawer: function()

Source/devtools/front_end/components/InspectorView.js
@@ -550,6 +550,8 @@ WebInspector.InspectorView.ToggleDrawerButtonProvider.prototype = {
      */
     item: function()
     {
-        return WebInspector.inspectorView._drawer.toggleButton();
+        if (!Runtime.experiments.isEnabled("mainMenu"))
+            return WebInspector.inspectorView._drawer.toggleButton();
+        return null;
     }
 }

Source/devtools/front_end/main/Main.js
@@ -801,10 +801,11 @@ WebInspector.Main.MainMenuItem.prototype = {
             true,
             this._item.element.totalOffsetLeft(),
             this._item.element.totalOffsetTop() + this._item.element.offsetHeight);
-        var dockItemElement = createElementWithClass("div", "flex-centered");
-        dockItemElement.createTextChild(WebInspector.UIString("Dock side"));
+        var dockItemElement = createElementWithClass("div", "flex-centered flex-auto");
+        var titleElement = dockItemElement.createChild("span", "flex-auto");
+        titleElement.textContent = WebInspector.UIString("Dock side");
+        dockItemElement.appendChild(titleElement);
         var dockItemToolbar = new WebInspector.Toolbar(dockItemElement);
-        dockItemToolbar.element.style.paddingLeft = "10px";
         var undock = new WebInspector.ToolbarButton( WebInspector.UIString("Undock into separate window"), "dock-toolbar-item-undock");
         var bottom = new WebInspector.ToolbarButton(WebInspector.UIString("Dock to bottom"), "dock-toolbar-item-bottom");
         var right = new WebInspector.ToolbarButton(WebInspector.UIString("Dock to right"), "dock-toolbar-item-right");

Source/devtools/front_end/ui/ContextMenu.js
@@ -90,6 +90,8 @@ WebInspector.ContextMenuItem.prototype = {
             var result = { type: "item", id: this._id, label: this._label, enabled: !this._disabled };
             if (this._customElement)
                 result.element = this._customElement;
+            if (this._shortcut)
+                result.shortcut = this._shortcut;
             return result;
         case "separator":
             return { type: "separator" };
@@ -97,6 +99,14 @@ WebInspector.ContextMenuItem.prototype = {
             return { type: "checkbox", id: this._id, label: this._label, checked: !!this._checked, enabled: !this._disabled };
         }
         throw new Error("Invalid item type:"  + this._type);
+    },
+
+    /**
+     * @param {string} shortcut
+     */
+    setShortcut: function(shortcut)
+    {
+        this._shortcut = shortcut;
     }
 }
 
@@ -148,7 +158,11 @@ WebInspector.ContextSubMenuItem.prototype = {
      */
     appendAction: function(label, actionId)
     {
-        return this.appendItem(label, WebInspector.actionRegistry.execute.bind(WebInspector.actionRegistry, actionId));
+        var result = this.appendItem(label, WebInspector.actionRegistry.execute.bind(WebInspector.actionRegistry, actionId));
+        var shortcut = WebInspector.shortcutRegistry.shortcutTitleForAction(actionId);
+        if (shortcut)
+            result.setShortcut(shortcut);
+        return result;
     },
 
     /**

Source/devtools/front_end/ui/ShortcutRegistry.js
@@ -61,6 +61,17 @@ WebInspector.ShortcutRegistry.prototype = {
     },
 
     /**
+     * @param {string} actionId
+     * @return {string|undefined}
+     */
+    shortcutTitleForAction: function(actionId)
+    {
+        var descriptors = this.shortcutDescriptorsForAction(actionId);
+        if (descriptors.length)
+            return descriptors[0].name;
+    },
+
+    /**
      * @param {!KeyboardEvent} event
      */
     handleShortcut: function(event)

commit 0564bf51c1e3d97407170dee95a5f9c3712d9717

DevTools: do not focus tabs upon all user gestures (regressed in r199533).
TBR=dgozman
BUG=515416

Review URL: https://codereview.chromium.org/1271183003

git-svn-id: svn://svn.chromium.org/blink/trunk@200199 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Source/devtools/front_end/sources/AdvancedSearchView.js
@@ -361,6 +361,7 @@ WebInspector.AdvancedSearchView.ActionDelegate.prototype = {
             WebInspector.inspectorView.setCurrentPanel(WebInspector.SourcesPanel.instance());
             this._searchView._toggle(queryCandidate);
             WebInspector.inspectorView.showCloseableViewInDrawer("sources.search", WebInspector.UIString("Search"), this._searchView);
+            this._searchView.focus();
         }
     }
 }

Source/devtools/front_end/ui/TabbedPane.js
@@ -340,11 +340,8 @@ WebInspector.TabbedPane.prototype = {
         var tab = this._tabsById[id];
         if (!tab)
             return false;
-        if (this._currentTab && this._currentTab.id === id) {
-            if (userGesture)
-                this.focus();
+        if (this._currentTab && this._currentTab.id === id)
             return true;
-        }
 
         this._hideCurrentTab();
         this._showTab(tab);
@@ -354,7 +351,7 @@ WebInspector.TabbedPane.prototype = {
         this._tabsHistory.splice(0, 0, tab);
 
         this._updateTabElements();
-        if (focused || userGesture)
+        if (focused)
             this.focus();
 
         var eventData = { tabId: id, view: tab.view, isUserGesture: userGesture };